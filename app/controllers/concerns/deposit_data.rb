fc715b2149d6addf9382b32f006f8c8e421dffe1
Fergus McGlynn <theferg@gmail.com>
2016-08-07 11:32:53 +0000

    Handle uploaded .zip files - extract contents to the transfer directory

--- app/controllers/concerns/deposit_data.rb
+++ app/controllers/concerns/deposit_data.rb
@@ -3,6 +3,7 @@ module DepositData
   extend ActiveSupport::Concern
 
   require 'http_headers'
+  require 'zip'
 
   included do
 
@@ -24,21 +25,34 @@ module DepositData
   end
 
   def deposit_files(file)
-    # TODO Allow multiple files
-    # TODO Unpack zip files
     # TODO Google Drive implementation
+
+    # handle each of the uploaded files
     file.each do |f|
       # get the uploaded filename (including its path in the case of directory upload)
       h = HttpHeaders.new(f.headers)
       uploaded_filename = h.content_disposition.match(/filename=(\"?)(.+)\1/)[2] 
-      # work out where this uploaded file should go (in order to preserve the structure of the upload)
-      target_file = File.join(@dir_aip, uploaded_filename)
-      target_dir = File.dirname(target_file)
-      # create any directories in target_dir that don't already exist
-      FileUtils.mkdir_p(target_dir)
-      # move this uploaded file into place (into target_dir)
-      FileUtils.chmod 0644, f.tempfile
-      FileUtils.mv(f.tempfile, target_file)
+      # if it's a .zip file, extract its contents to @dir_aip
+      if File.extname(uploaded_filename) == '.zip' then
+        Zip::File.open(f.tempfile) do |zip_file|
+          zip_file.each do |entry|
+            # extract everything except mac osx guff
+            unless entry.name.include?("__MACOSX") then
+              entry.extract(File.join(@dir_aip, entry.name))
+            end
+          end
+        end
+      # otherwise, not a zip file, just bung this file in the @dir_aip folder
+      else
+        # work out where this uploaded file should go (in order to preserve the structure of the upload)
+        target_file = File.join(@dir_aip, uploaded_filename)
+        target_dir = File.dirname(target_file)
+        # create any directories in target_dir that don't already exist
+        FileUtils.mkdir_p(target_dir)
+        # move this uploaded file into place (into target_dir)
+        FileUtils.chmod 0644, f.tempfile
+        FileUtils.mv(f.tempfile, target_file)
+      end
     end
   end
 
