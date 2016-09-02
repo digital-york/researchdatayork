9951536d109a28f37e59b45e4df844978756bbf0
Julie Allinson <julie.allinson@york.ac.uk>
2016-09-02 11:38:29 +0100

    config changes

--- startapps.sh
+++ startapps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-fcrepo_wrapper -p 8984 & disown
-echo 'waiting 120'
-sleep 120
+./fedstart.sh & disown
+echo 'waiting 1 min'
+sleep 60
 disown;
-solr_wrapper -d solr/config/ --collection_name hydra_works --version 6.1.0 & disown
-echo 'waiting 120'
-sleep 120
+./solrstart.sh & disown
+#echo 'waiting 1 min'
+#sleep 60
 disown;
-rails s -b 0.0.0.0 & disown
-wait 20
-disown;
+#rails s -b 0.0.0.0 & disown
+#wait 20
+#disown;
