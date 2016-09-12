1ddf70a2a5a9232424dd3e27b6c630f4f289f08b
Julie Allinson <julie.allinson@york.ac.uk>
2016-09-12 17:27:26 +0100

    merging

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -20,12 +20,10 @@ class DepositsController < ApplicationController
   # GET /deposits
   # GET /deposits.json
   def index
+
     # This is an empty ActiveRecord object. It is never saved.
     @deposit = Deposit.new
 
-<<<<<<< HEAD
-    # Setup base query parameters.
-=======
     # if user asked for new/updated datasets, fetch or update them
     if params[:refresh] == 'true'
       # Get number of results to return
@@ -57,20 +55,15 @@ class DepositsController < ApplicationController
     end
 
     # setup base query parameters
->>>>>>> 3511ab699a0b29082cf9ab9676db4b81ef7cfee4
     q = 'has_model_ssim:"Dlibhydra::Dataset"'
     ids = []
     fq = []
     no_results = false
 
     unless params[:q].nil?
-      # TODO: or search for multiple words etc.
-      # Search  PURE metadata (for_indexing), restrictions notes
-      #   and dataset (hydra) id.
+      # TODO or search for multiple words etc.
       unless params[:q] == ''
-        fq << "for_indexing_tesim:#{params[:q]}
-                OR restriction_note_tesim:#{params[:q]}
-                OR id:#{params[:q]}"
+        fq << 'for_indexing_tesim:' + params[:q] + ' OR restriction_note_tesim:' + params[:q]
       end
 
       unless params[:new].nil?
@@ -94,11 +87,12 @@ class DepositsController < ApplicationController
 
       unless params[:aip_status].nil?
         params[:aip_status].each do |aipstatus|
+
           if aipstatus == 'noaip'
             fq << '!member_ids_ssim:*'
           else
             fq << 'member_ids_ssim:*'
-            num_results = get_number_of_results('has_model_ssim:"Dlibhydra::Dataset" and member_ids_ssim:*')
+            num_results = get_number_of_results('has_model_ssim:"Dlibhydra::Dataset" and member_ids_ssim:*',)
             if num_results == 0
               fq << 'member_ids_ssim:*'
             else
@@ -113,7 +107,7 @@ class DepositsController < ApplicationController
               end
               r['docs'].each do |dataset|
                 dataset['member_ids_ssim'].each do |aip|
-                  num_results = get_number_of_results('id:' + aip, status_query)
+                  num_results = get_number_of_results('id:'+ aip, status_query)
                   if num_results == 0
                     fq << '!id:' + dataset['id']
                   else
@@ -130,7 +124,7 @@ class DepositsController < ApplicationController
     unless params[:dip_status].nil?
       no_results = true
       params[:dip_status].each do |dipstatus|
-        num_results = get_number_of_results('has_model_ssim:"Dlibhydra::Dataset" and member_ids_ssim:*')
+        num_results = get_number_of_results('has_model_ssim:"Dlibhydra::Dataset" and member_ids_ssim:*',)
         if num_results == 0
           fq << 'member_ids_ssim:*'
         else
@@ -138,8 +132,8 @@ class DepositsController < ApplicationController
                                 'id,member_ids_ssim', num_results)
           r['docs'].each do |dataset|
             dataset['member_ids_ssim'].each do |dip|
-              if dipstatus == 'APPROVE' || dipstatus == 'UPLOADED'
-                num_results = get_number_of_results('id:' + dip + ' and dip_status_tesim:' + dipstatus, [])
+              if dipstatus == 'APPROVE' or dipstatus == 'UPLOADED'
+                num_results = get_number_of_results('id:'+ dip +' and dip_status_tesim:' + dipstatus, [])
                 if num_results == 0
                   fq << '!id:' + dataset['id']
                 else
@@ -147,7 +141,7 @@ class DepositsController < ApplicationController
                   no_results = false
                 end
               elsif dipstatus == 'waiting'
-                num_results = get_number_of_results('id:' + dip, ['requestor_email_tesim:*', '!dip_status_tesim:*'])
+                num_results = get_number_of_results('id:'+ dip, ['requestor_email_tesim:*', '!dip_status_tesim:*'])
                 if num_results == 0
                   fq << '!id:' + dataset['id']
                 else
@@ -155,7 +149,7 @@ class DepositsController < ApplicationController
                   no_results = false
                 end
               else
-                num_results = get_number_of_results('id:' + dip + ' and dip_uuid_tesim:*')
+                num_results = get_number_of_results('id:'+ dip +' and dip_uuid_tesim:*')
                 unless num_results == 0
                   no_results = false
                   fq << '!id:' + dataset['id']
@@ -170,11 +164,12 @@ class DepositsController < ApplicationController
     unless ids.empty?
       extra_fq = 'id:('
       ids.each_with_index do |i, index|
-        extra_fq += if index == ids.length - 1
-                      i.to_s
-                    else
-                      "#{i} or "
-                    end
+        if index == ids.length - 1
+          extra_fq += "#{i}"
+        else
+          extra_fq += "#{i} or "
+        end
+
       end
       extra_fq += ')'
       fq << extra_fq
@@ -193,7 +188,7 @@ class DepositsController < ApplicationController
       elsif params[:sort] == 'created'
         solr_sort = 'pure_creation_ssi'
       elsif params[:sort] == 'available'
-        solr_sort = 'date_available_ssi' 
+        solr_sort = 'date_available_ssi'
       end
       # if a valid sort direction was given, include that in the sort clause
       if params[:sort_order] and ["asc","desc"].include?(params[:sort_order]) then
@@ -226,36 +221,12 @@ class DepositsController < ApplicationController
       end
     end
 
-<<<<<<< HEAD
-    if params[:refresh] == 'true'
-      if params[:refresh_num]
 
-        c = get_uuids(params[:refresh_num])
-        get_datasets_from_collection(c, response)
-
-      elsif params[:refresh_from]
-
-        c = get_uuids_created_from_tonow(params[:refresh_from])
-        uuids = get_datasets_from_collection(c, response)
-
-        # uuids is a list of new datasets created
-        # these are used to ensure we don't create duplicates with the modified records
-        c = get_uuids_modified_from_tonow(params[:refresh_from])
-        get_datasets_from_collection(c, response, uuids)
-
-      else
-        c = get_uuids
-        get_datasets_from_collection(c, response)
-      end
+    if response.nil?
+      @deposits = []
+    else
+      @deposits = response
     end
-=======
->>>>>>> 3511ab699a0b29082cf9ab9676db4b81ef7cfee4
-
-    @deposits = if response.nil?
-                  []
-                else
-                  response
-                end
     respond_to do |format|
       format.html { render :index }
       # format.json { render :index, status: :created, location: @dataset }
@@ -265,16 +236,16 @@ class DepositsController < ApplicationController
   # GET /deposits/1
   # GET /deposits/1.json
   def show
+
     @notice = ''
 
     if params[:deposit]
       # if the user uploaded local file(s), they will be in params[:deposit][:file], if cloud file(s), they'll be in params[:selected_files]
-      if params[:deposit][:file] || params[:selected_files]
-        @aip = create_aip
-        user_deposit(@dataset, params[:deposit][:readme])
+      if params[:deposit][:file] or params[:selected_files]
+        @aip = new_aip
+        set_user_deposit(@dataset, params[:deposit][:readme])
         new_deposit(@dataset.id, @aip.id)
         add_metadata(@dataset.for_indexing)
-
         begin
           # handle readme (submission documentation)
           if params[:deposit][:readme] and !params[:deposit][:readme].empty?
@@ -285,19 +256,19 @@ class DepositsController < ApplicationController
             deposit_files_from_client(params[:deposit][:file])
           end
           # handle upload of google drive file(s)
-          if params[:selected_files] && params[:selected_paths] && params[:selected_mimetypes]
+          if params[:selected_files] and params[:selected_paths] and params[:selected_mimetypes]
             deposit_files_from_cloud(params[:selected_files], params[:selected_paths], params[:selected_mimetypes])
           end
-        # if there was problem uploading files, delete the new AIP and delete any files that did get uploaded
+            # if there was problem uploading files, delete the new AIP and delete any files that did get uploaded
         rescue => e
-          # delete the aip from the aips list
+          # delete from aips membership
           @dataset.aips.delete(@dataset.aips.last)
-          # then delete the aip object
+          # delete aip
           delete_aip
           delete_deposited_files
           @notice = 'Failed to deposit selected files: ' + e.message
         else
-          # TODO: write metadata.json
+          # TODO write metadata.json
           # TODO add submission info
           @notice = 'The deposit was successful.'
           @dataset = nil
@@ -326,6 +297,7 @@ class DepositsController < ApplicationController
   # POST /deposits
   # POST /deposits.json
   def create
+
     # If a pure uuid has been supplied
     if params[:deposit]
       if params[:deposit][:pure_uuid]
@@ -334,7 +306,7 @@ class DepositsController < ApplicationController
         uuid = params[:deposit][:pure_uuid]
         d = get_pure_dataset(uuid)
 
-        unless d.nil? or d['uuid'].nil?
+        unless d.nil?
           query = 'pure_uuid_tesim:"' + d['uuid'] + '""'
           response = solr_query_short(query, 'id,pure_uuid_tesim', 1)
 
@@ -362,8 +334,11 @@ class DepositsController < ApplicationController
         @deposit.id = params[:deposit][:id].to_s
         @dataset_id = params[:deposit][:id].to_s
         d = Dlibhydra::Dataset.find(@dataset_id)
-        d.wf_status = params[:deposit][:status]
+        if params[:deposit][:status]
+          d.wf_status = params[:deposit][:status]
+        end
         if params[:deposit][:retention_policy]
+
           d.retention_policy = params[:deposit][:retention_policy]
         end
         if params[:notes]
@@ -382,9 +357,9 @@ class DepositsController < ApplicationController
         @deposit.notes = d.restriction_note
 
         respond_to do |format|
-          # format.html { render :show, notice: notice }
+          #format.html { render :show, notice: notice }
           format.js
-          # format.json { render :show, status: :created, location: @deposit }
+          #format.json { render :show, status: :created, location: @deposit }
         end
       end
     end
@@ -421,6 +396,15 @@ class DepositsController < ApplicationController
     # TODO
   end
 
+  # Notes
+  def notes
+    respond_to do |format|
+      #format.html { render :show, notice: notice }
+      format.js {}
+      #format.json { render :show, status: :created, location: @deposit }
+    end
+  end
+
   # Reingest
   def reingest
     message = reingest_aip('objects', params[:id])
@@ -431,7 +415,7 @@ class DepositsController < ApplicationController
   end
 
   def dipuuid
-    message = update_dip(params[:deposit][:id], params[:deposit][:dipuuid])
+    message = update_dip(params[:deposit][:id],params[:deposit][:dipuuid])
     # data (DIP) is now available so send an email to anyone who requested the data
     RdMailer.notify_requester(params[:deposit][:id]).deliver_now
     respond_to do |format|
@@ -441,7 +425,6 @@ class DepositsController < ApplicationController
   end
 
   private
-
   # Use callbacks to share common setup or constraints between actions.
   def set_deposit
     @deposit = Deposit.new
@@ -453,27 +436,30 @@ class DepositsController < ApplicationController
     params.permit(:deposit, :uuid, :file, :submission_doco,
                   :title, :refresh, :refresh_num, :refresh_from,
                   :pure_uuid, :readme, :access,
-                  :embargo_end, :available, :dipuuid, :status, :release, :q, :aip_status, :dip_status, :doi, :retention_policy, :notes)
+                  :embargo_end, :available, :dipuuid, :status, :release, :q, :aip_status, :dip_status, :doi,:retention_policy, :notes)
   end
 
   private
 
-  # Given a Puree collection (an array of hashes), get each dataset,
-  # create a new Hydra dataset, or update an existing one.
-  # Ignore data not published by the default publisher.
-  def get_datasets_from_collection(c, response, new_uuids = [])
-    c.each do |d|
-      next if d['publisher'].exclude? ENV['PUBLISHER']
-      if !response.nil? && (new_uuids.include?(d['uuid']) || response.to_s.include?(d['uuid']))
-        r = solr_query_short('pure_uuid_tesim:"' + d['uuid'] + '"', 'id', 1)
-        local_d = find_dataset(r['docs'][0]['id'])
-      else
-        new_uuids << d['uuid']
-        local_d = new_dataset
-      end
+  # Given a Puree collection (an array of hashes), get each dataset
+  # Create a new Hydra dataset, or update an existing one
+  # Ignore data not published by the given publisher
+  def get_datasets_from_collection(c, response, new_uuids=[])
 
-      set_metadata(local_d, d)
+    c.each do |d|
+      unless d['publisher'].exclude? ENV['PUBLISHER']
+        if response != nil and (new_uuids.include? d['uuid'] or response.to_s.include? d['uuid'])
+          r = solr_query_short('pure_uuid_tesim:"' + d['uuid'] + '"', 'id', 1)
+          local_d = find_dataset(r['docs'][0]['id'])
+        else
+          new_uuids << d['uuid']
+          local_d = new_dataset
+        end
+
+        set_metadata(local_d, d)
+      end
     end
     new_uuids
   end
+
 end
