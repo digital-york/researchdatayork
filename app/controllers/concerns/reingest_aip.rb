9d1ceed9434bfa627d01bf49566350afdfc18e47
Fergus McGlynn <theferg@gmail.com>
2016-09-13 11:45:19 +0100

    added exception handling to process of calling Archivematica API for reingest

--- app/controllers/concerns/reingest_aip.rb
+++ app/controllers/concerns/reingest_aip.rb
@@ -12,16 +12,20 @@ module ReingestAip
     dataset = Dlibhydra::Dataset.find(id)
     aip = dataset.aips[0]
     dip = dataset.aips[0]
-    dip.dip_status = 'APPROVE'
-    dip.save
 
     unless aip.aip_uuid.nil?
 
       url = ENV['ARCHIVEMATICA_SS_URL']
-      conn = Faraday.new(:url => url) do |faraday|
-        faraday.request  :url_encoded             # form-encode POST params
-        faraday.response :logger                  # log requests to STDOUT
-        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      begin
+        conn = Faraday.new(:url => url) do |faraday|
+          faraday.request  :url_encoded             # form-encode POST params
+          faraday.response :logger                  # log requests to STDOUT
+          faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+        end
+      rescue => e
+        Rails.logger.error "Error in concerns/reingest_aip.rb#reingest_aip (unable to connect to Archivematica). Error message: " + e.message
+        flash[:error] = "Unable to connect to Archivematica. Please try again later."
+        return ""
       end
 
       path = '/api/v2/file/' + aip.aip_uuid + '/reingest/'
@@ -34,14 +38,29 @@ module ReingestAip
           'api_key' => ENV['ARCHIVEMATICA_SS_API_KEY']
       }
 
-      response = conn.post do |req|
-        req.url path
-        req.headers['Content-Type'] = 'application/json'
-        req.params = params
-        req.body = body
-      end
+      begin
+        response = conn.post do |req|
+          req.url path
+          req.headers['Content-Type'] = 'application/json'
+          req.params = params
+          req.body = body
+        end
+      rescue => e
+        Rails.logger.error "Error in concerns/reingest_aip.rb#reingest_aip (problem calling Archivematica API). Error message: " + e.message
+        flash[:error] = "Unable to connect to Archivematica. Please try again later."
+        return ""
+      end 
 
-      # TODO some error handling!
+      # make sure that the response was 200 OK
+      if response.status == "200"
+        # now that reingest request has been made successfully, set dip status to approved
+        dip.dip_status = 'APPROVE'
+        dip.save
+      else
+        Rails.logger.error "Error in concerns/reingest_aip.rb#reingest_aip - response from Archivematica API was " + response.status + " (expecting 200). Response: " + response.body
+        flash[:error] = "Unexpected response from Archivematica. Make sure the Archivematica credentials are valid and that the dataset exists in Archivematica"
+        return ""
+      end
       # TODO abstract faraday for re-use
       JSON.parse(response.body)
 
