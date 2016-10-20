52b0739f69bd4ce2fe21584a58f1fa1f09893906
Julie Allinson (tmp) <julieallinson@Julies-MacBook-Pro.local>
2016-10-20 16:28:34 +0100

    local changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable,
-         #         :registerable,
+         :registerable,
          #         :recoverable,
          :rememberable,
          :trackable,
