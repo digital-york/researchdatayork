4ddef0d8cb58a227e349c530a04a094b7be57316
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2016-12-07 13:57:00 +0000

    prepend commands with 'bundle exec' to make it explicit how to run commands

--- startup/fedstart.sh
+++ startup/fedstart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # rm -r tmp/fcrepo4-data
-fcrepo_wrapper -p 8984 --instance_directory ../tmp/fcrepo --fcrepo_home ~/tmp/fcrepo4-data
+bundle exec fcrepo_wrapper -p 8984 --instance_directory ../tmp/fcrepo --fcrepo_home ~/tmp/fcrepo4-data
