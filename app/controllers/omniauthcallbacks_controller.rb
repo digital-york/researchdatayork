2bb27e98b878be6f6a749d29daebaaf42d4bf3de
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2016-12-10 13:21:00 +0000

    make a user an 'admin' user if they've got the right shib attributes

--- app/controllers/omniauthcallbacks_controller.rb
+++ app/controllers/omniauthcallbacks_controller.rb
@@ -5,11 +5,15 @@ class OmniauthcallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     # capture data about the user from shib
     session['shib_user_data'] = request.env["omniauth.auth"]
+    # if the shib data indicates that this person is a member of staff in information systems, make them an admin
+    if request.env["omniauth.auth"].info[:affiliation].include?("Staff")
+      @user.update_attribute :admin, true
+    end
     sign_in_and_redirect @user
 
     # for debugging - comment out "sign_in_and_redirect" line above and uncomment the following two lines
-    # sign_in @user
-    #render :plain => request.env["omniauth.auth"]
+    #sign_in @user
+    #render :plain => "data: " + request.env["omniauth.auth"].to_s
   end
 
   # when shib login fails
