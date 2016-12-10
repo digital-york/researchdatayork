2dc8867a4c17f3b22ab42aa02958ac65e6452f56
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2016-12-10 11:25:06 +0000

    adding 'admin' attribute to user model - initially, infosys staff will be admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207130611) do
+ActiveRecord::Schema.define(version: 20161210112258) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20161207130611) do
     t.boolean  "guest",                  default: false
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
