4a4d6e19beac34058ce62c770491906e0d613907
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2016-12-10 12:05:05 +0000

    minor formatting tweak

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable,
          # :registerable,
-         #         :recoverable,
+         # :recoverable,
          :rememberable,
          :trackable,
          :validatable,
