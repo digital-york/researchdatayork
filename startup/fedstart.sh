6a3fafa77e745fad3939dc03168ce21e9d1a6428
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2016-12-15 12:59:39 +0000

    setting fedora and solr data locations to /var/tmp as ~/tmp sometimes resulted in data being deleted

--- startup/fedstart.sh
+++ startup/fedstart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # rm -r tmp/fcrepo4-data
-bundle exec fcrepo_wrapper -p 8984 --instance_directory ../tmp/fcrepo --fcrepo_home ~/tmp/fcrepo4-data
+#bundle exec fcrepo_wrapper -p 8984 --instance_directory ../tmp/fcrepo --fcrepo_home ~/tmp/fcrepo4-data
+bundle exec fcrepo_wrapper -p 8984 --instance_directory /var/tmp/fcrepo --fcrepo_home /var/tmp/fcrepo4-data
